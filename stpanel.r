42bba86546ed8956332c8298c1dda0de819f6333
David Lambert <lambert.david.0@gmail.com>
2019-04-21 13:08:14 -0400

    Created 9 scatterplots on common scales: VEP, VAP, ACS VEP X 2012, 2012, Pooled

--- stpanel.r
+++ stpanel.r
@@ -446,7 +446,8 @@ stpan <- left_join(stpan, turnout, by = c("st" = "st", "year" = "year"))
 stpan <- stpan %>% 
   mutate(toacs = presvotes / vapacs)
 
-
+# cleanup
+rm(list = c("to12", "to16", "turnout"))
 
 # BASIC CORRELATIONS ====
 
@@ -490,4 +491,335 @@ stpan <- stpan %>%
   cor(stpan$hdi, stpan$tovap)
   cor(stpan$hdi, stpan$toacs)
   
+
   
+# 2012 SCATTERPLOTS ====
+
+# 2012 VEP
+scat12_vep <-   
+  stpan %>% 
+    filter(year == "2012") %>% 
+    ggplot(aes(x = hdi, y = tovep)) +
+      geom_point(
+        shape = 16,
+        alpha = .7,
+        size = 4,
+        stroke = 0,
+        color = "grey70"
+      ) +
+      geom_smooth(
+        se = FALSE,
+        color = "orange",
+        size = 1.5,
+        method = "loess"
+      ) +
+      geom_smooth(
+        se = FALSE,
+        color = "seagreen",
+        size = 1.5,
+        method = "lm"
+      ) +
+      scale_x_continuous(limits = c(4,7.25)) +
+      scale_y_continuous(limits = c(.35, .8)) +
+      theme_minimal() +
+      labs(
+        title = "2012 VEP",
+        x = "HDI",
+        y = "Turnout"
+      )
+scat12_vep  
+
+# 2012 VAP
+scat12_vap <-   
+  stpan %>% 
+    filter(year == "2012") %>% 
+    ggplot(aes(x = hdi, y = tovap)) +
+      geom_point(
+        shape = 16,
+        alpha = .7,
+        size = 4,
+        stroke = 0,
+        color = "grey70"
+      ) +
+      geom_smooth(
+        se = FALSE,
+        color = "orange",
+        size = 1.5,
+        method = "loess"
+      ) +
+      geom_smooth(
+        se = FALSE,
+        color = "seagreen",
+        size = 1.5,
+        method = "lm"
+      ) +
+      scale_x_continuous(limits = c(4,7.25)) +
+      scale_y_continuous(limits = c(.35, .8)) +
+      theme_minimal() +
+      labs(
+        title = "2012 VAP",
+        x = "HDI",
+        y = "Turnout"
+      )
+scat12_vap  
+
+# 2012 ACS VEP
+scat12_acs <-   
+  stpan %>% 
+    filter(year == "2012") %>% 
+    ggplot(aes(x = hdi, y = toacs)) +
+      geom_point(
+        shape = 16,
+        alpha = .7,
+        size = 4,
+        stroke = 0,
+        color = "grey70"
+      ) +
+      geom_smooth(
+        se = FALSE,
+        color = "orange",
+        size = 1.5,
+        method = "loess"
+      ) +
+      geom_smooth(
+        se = FALSE,
+        color = "seagreen",
+        size = 1.5,
+        method = "lm"
+      ) +
+      scale_x_continuous(limits = c(4,7.25)) +
+      scale_y_continuous(limits = c(.35, .8)) +
+      theme_minimal() +
+      labs(
+        title = "2012 ACS VEP",
+        x = "HDI",
+        y = "Turnout"
+      )
+scat12_acs  
+
+
+
+# 2016 SCATTERPLOTS ====
+
+
+# 2016 VEP
+scat16_vep <-   
+  stpan %>% 
+    filter(year == "2016") %>% 
+    ggplot(aes(x = hdi, y = tovep)) +
+      geom_point(
+        shape = 16,
+        alpha = .7,
+        size = 4,
+        stroke = 0,
+        color = "grey70"
+      ) +
+      geom_smooth(
+        se = FALSE,
+        color = "orange",
+        size = 1.5,
+        method = "loess"
+      ) +
+      geom_smooth(
+        se = FALSE,
+        color = "seagreen",
+        size = 1.5,
+        method = "lm"
+      ) +
+      scale_x_continuous(limits = c(4,7.25)) +
+      scale_y_continuous(limits = c(.35, .8)) +
+      theme_minimal() +
+      labs(
+        title = "2016 VEP",
+        x = "HDI",
+        y = "Turnout"
+      )
+scat16_vep  
+
+# 2016 VAP
+scat16_vap <-   
+  stpan %>% 
+    filter(year == "2016") %>% 
+    ggplot(aes(x = hdi, y = tovap)) +
+      geom_point(
+        shape = 16,
+        alpha = .7,
+        size = 4,
+        stroke = 0,
+        color = "grey70"
+      ) +
+      geom_smooth(
+        se = FALSE,
+        color = "orange",
+        size = 1.5,
+        method = "loess"
+      ) +
+      geom_smooth(
+        se = FALSE,
+        color = "seagreen",
+        size = 1.5,
+        method = "lm"
+      ) +
+      scale_x_continuous(limits = c(4,7.25)) +
+      scale_y_continuous(limits = c(.35, .8)) +
+      theme_minimal() +
+      labs(
+        title = "2016 VAP",
+        x = "HDI",
+        y = "Turnout"
+      )
+scat16_vap  
+
+# 2016 ACS VEP
+scat16_acs <-   
+  stpan %>% 
+    filter(year == "2016") %>% 
+    ggplot(aes(x = hdi, y = toacs)) +
+      geom_point(
+        shape = 16,
+        alpha = .7,
+        size = 4,
+        stroke = 0,
+        color = "grey70"
+      ) +
+      geom_smooth(
+        se = FALSE,
+        color = "orange",
+        size = 1.5,
+        method = "loess"
+      ) +
+      geom_smooth(
+        se = FALSE,
+        color = "seagreen",
+        size = 1.5,
+        method = "lm"
+      ) +
+      scale_x_continuous(limits = c(4,7.25)) +
+      scale_y_continuous(limits = c(.35, .8)) +
+      theme_minimal() +
+      labs(
+        title = "2016 ACS VEP",
+        x = "HDI",
+        y = "Turnout"
+      )
+scat16_acs  
+
+
+
+
+# POOLED SCATTERPLOTS ====
+
+# grey pallets
+greys <- c("grey70", "grey30")
+
+# Pooled VEP
+pooled_vep <-   
+  stpan %>% 
+    ggplot(aes(x = hdi, y = tovep, color = year)) +
+      geom_point(
+        shape = 16,
+        alpha = .7,
+        size = 4,
+        stroke = 0
+      ) +
+      scale_color_manual(values = greys) +
+      geom_smooth(
+        se = FALSE,
+        color = "orange",
+        size = 1.5,
+        method = "loess"
+      ) +
+      geom_smooth(
+        se = FALSE,
+        color = "seagreen",
+        size = 1.5,
+        method = "lm"
+      ) +
+      scale_x_continuous(limits = c(4,7.25)) +
+      scale_y_continuous(limits = c(.35, .8)) +
+      theme_minimal() +
+      theme(
+        legend.position = c(.8, .25) ,
+        legend.title = element_blank()
+      ) +
+      labs(
+        title = "Pooled VEP",
+        x = "HDI",
+        y = "Turnout"
+      )
+pooled_vep  
+
+
+# Pooled VAP
+pooled_vap <-   
+  stpan %>% 
+    ggplot(aes(x = hdi, y = tovap, color = year)) +
+      geom_point(
+        shape = 16,
+        alpha = .7,
+        size = 4,
+        stroke = 0
+      ) +
+      scale_color_manual(values = greys) +
+      geom_smooth(
+        se = FALSE,
+        color = "orange",
+        size = 1.5,
+        method = "loess"
+      ) +
+      geom_smooth(
+        se = FALSE,
+        color = "seagreen",
+        size = 1.5,
+        method = "lm"
+      ) +
+      scale_x_continuous(limits = c(4,7.25)) +
+      scale_y_continuous(limits = c(.35, .8)) +
+      theme_minimal() +
+      theme(
+        legend.position = c(.8, .25) ,
+        legend.title = element_blank()
+      ) +
+      labs(
+        title = "Pooled VAP",
+        x = "HDI",
+        y = "Turnout"
+      )
+pooled_vap  
+
+# Pooled ACS VEP
+pooled_acs <-   
+  stpan %>% 
+    ggplot(aes(x = hdi, y = toacs, color = year)) +
+      geom_point(
+        shape = 16,
+        alpha = .7,
+        size = 4,
+        stroke = 0
+      ) +
+      scale_color_manual(values = greys) +
+      geom_smooth(
+        se = FALSE,
+        color = "orange",
+        size = 1.5,
+        method = "loess"
+      ) +
+      geom_smooth(
+        se = FALSE,
+        color = "seagreen",
+        size = 1.5,
+        method = "lm"
+      ) +
+      scale_x_continuous(limits = c(4,7.25)) +
+      scale_y_continuous(limits = c(.35, .8)) +
+      theme_minimal() +
+      theme(
+        legend.position = c(.8, .25) ,
+        legend.title = element_blank()
+      ) +
+      labs(
+        title = "Pooled ACS VEP",
+        x = "HDI",
+        y = "Turnout"
+      )
+pooled_acs  
