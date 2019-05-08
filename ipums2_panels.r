36069c32705b28a64359cca4034be90a146ce458
David Lambert <lambert.david.0@gmail.com>
2019-05-08 18:01:29 -0400

    added attainment, enrollment, overall education indices

--- ipums2_panels.r
+++ ipums2_panels.r
@@ -142,13 +142,146 @@ ctpan %>% ggplot() +
   geom_boxplot(aes(y = health_index)) +
   coord_flip() +
   facet_wrap(~year, ncol = 1) +
+  labs(title = "Health Index", y = "") +
   theme_minimal() +
   theme(
-    axis.text.y = element_blank()
+    axis.text.y = element_blank(),
+    plot.title = element_text(hjust = .05),
+    strip.text = element_text(hjust = .05),
+    panel.grid.major.y = element_blank(),
+    panel.grid.minor.y = element_blank()
   )
 
 
 # go with it!
 
+
+
+# ATTAINMENT INDEX ====
+
+# gather attainment "score"
+ctpan <- ctpan %>% 
+  mutate(
+    hsprop = (hsplus + bacc + grad) / pop25o,
+    baccprop = (bacc + grad) / pop25o,
+    gradprop = grad / pop25o,
+    attain_score = hsprop + baccprop + gradprop
+  )
+
+
+# check for utility of SSRA's goalposts
+summary(subset(ctpan, year == 2008, select = attain_score))
+summary(subset(ctpan, year == 2012, select = attain_score))
+summary(subset(ctpan, year == 2016, select = attain_score))
+# SSRA goalposts should be ok
+
+attmin <- 0.5
+attmax <- 2
+
+ctpan <- ctpan %>% 
+  mutate(
+   attain_index = ((attain_score - attmin) / (attmax - attmin)) * 10 
+  )
+
+
+# median a little high, but go with it
+summary(subset(ctpan, year == 2008, select = attain_index))
+summary(subset(ctpan, year == 2012, select = attain_index))
+summary(subset(ctpan, year == 2016, select = attain_index))
+
+
+
+# visualize
+# all have a few upper outliers, but not many
+ctpan %>% ggplot() +
+  geom_boxplot(aes(y = attain_index)) +
+  coord_flip() +
+  facet_wrap(~year, ncol = 1) +
+  labs(title = "Attainment Index", y = "") +
+  theme_minimal() +
+  theme(
+    axis.text.y = element_blank(),
+    plot.title = element_text(hjust = .05),
+    strip.text = element_text(hjust = .05),
+    panel.grid.major.y = element_blank(),
+    panel.grid.minor.y = element_blank()
+  )
+
+
+
+
+# ENROLLMENT INDEX ====
+
+ctpan <- ctpan %>% 
+  mutate(enrollprop = enroll / pop324)
+
+
+# check out
+summary(subset(ctpan, year == 2008, select = enrollprop))
+summary(subset(ctpan, year == 2012, select = enrollprop))
+summary(subset(ctpan, year == 2016, select = enrollprop))
+# 0.6-0.95 should be ok
+
+enrollmin <- 0.6
+enrollmax <- 0.95
+
+
+ctpan <- ctpan %>% 
+  mutate(
+    enroll_index = ( (enrollprop - enrollmin) / (enrollmax - enrollmin) ) * 10
+  )
+
+
+summary(subset(ctpan, year == 2008, select = enroll_index))
+summary(subset(ctpan, year == 2012, select = enroll_index))
+summary(subset(ctpan, year == 2016, select = enroll_index))
+# looks ok
+
+
+# visualize
+ctpan %>% ggplot() +
+  geom_boxplot(aes(y = enroll_index)) +
+  coord_flip() +
+  facet_wrap(~year, ncol = 1) +
+  labs(title = "Enrollment Index", y = "") +
+  theme_minimal() +
+  theme(
+    axis.text.y = element_blank(),
+    plot.title = element_text(hjust = .05),
+    strip.text = element_text(hjust = .05),
+    panel.grid.major.y = element_blank(),
+    panel.grid.minor.y = element_blank()
+  )
+
+
+
+# OVERALL EDUCATION INDEX ====
+
+ctpan <- ctpan %>% 
+  mutate(ed_index = ((2/3) * attain_index) + ((1/3) * enroll_index))
+
+summary(subset(ctpan, year == 2008, select = ed_index))
+summary(subset(ctpan, year == 2012, select = ed_index))
+summary(subset(ctpan, year == 2016, select = ed_index))
+# a little higher than 5, but probably OK
+
+
+# visualize
+ctpan %>% ggplot() +
+  geom_boxplot(aes(y = ed_index)) +
+  coord_flip() +
+  facet_wrap(~year, ncol = 1) +
+  labs(title = "Education Index", y = "") +
+  theme_minimal() +
+  theme(
+    axis.text.y = element_blank(),
+    plot.title = element_text(hjust = .05),
+    strip.text = element_text(hjust = .05),
+    panel.grid.major.y = element_blank(),
+    panel.grid.minor.y = element_blank()
+  )
+
+
 sdfg
+
 
